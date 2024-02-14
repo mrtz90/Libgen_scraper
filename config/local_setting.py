95399a34c96ef2b84c9b09fbaefa951436d30830
mrtz90 <morteza48.ahmadi@gmail.com>
2024-02-14 15:41:05 +0330

    create a django project with two app scraper and libgen.

---
+++ config/local_setting.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ephyt4t=e18mh!x9-3xiyxz%#w(=c7e=dkgd5h=ptfqajly+ki'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ config/sample_setting.py
@@ -0,0 +1,9 @@
+# At first create local_setting.py and put this information on it.
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '<enter you secret key here>'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
